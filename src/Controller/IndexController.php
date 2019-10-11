371f8ac2ef4653acd621d01416da06137fec02e1
radovan.barnabas <r.barnabas96@gmail.com>
2019-10-11 11:17:54 +0200

    Random number feature

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -11,49 +11,12 @@ class IndexController
      */
     public static function IndexAction()
     {
-        $url = 'http://127.0.0.1:8000/hello';
-        /*
-        try
-        {
-            $json = file_get_contents($url);
-            $obj = json_decode($json);
-        }
-        catch(Exception $e)
-        {
-            echo $e;
-        }
-        */
-
-        
-        $curl_handle = curl_init();
-        curl_setopt($curl_handle, CURLOPT_URL, $url);
-        curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
-        curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
-        $query = curl_exec($curl_handle);
-        curl_close($curl_handle);
-
-        $obj = json_decode($query);
-
-        return new Response(
-            '<html>
-                <body>
-                    <p>'.$obj.'</p>
-                </body>
-            </html>'
-        );
-    }
-
-    /**
-     * @Route("/{index}")
-     */
-    public function SpecificElement(int $index)
-    {
-        return new Response(
-            '<html>
-                <body>
-                    <p>' .$index. '</p>
-                </body>
-            </html>'
-        );
+        return new Response('
+        <html>
+            <body>
+                <p>Hi! Use one of my functions to get dynamic, read-only data!</p>
+                <a href="/random">Array of random numbers.</a> You can add an extra number at the end for how many do you need. The default value is 10.
+            </body>
+        </html>');
     }
 }
---
+++ src/Controller/RandomController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RandomController 
+{
+    /**
+     * @Route("/random")
+     */
+    public function RandomAction()
+    {
+        $random_numbers = array();
+        for($i = 0; $i < 10; $i++)
+        {
+            $random_numbers[$i] = random_int(0,10);
+        }
+        $response = new JsonResponse(array('Array of random numbers' => $random_numbers));
+
+        return $response;
+    }
+
+    /**
+     * @Route("/random/{size}")
+     */
+    public function CusmonSizeRandomAction(int $size)
+    {
+        $random_numbers = array();
+        for ($i = 0; $i < $size; $i++) {
+            $random_numbers[$i] = random_int(0, 10);
+        }
+        $response = new JsonResponse(array('Array of random numbers' => $random_numbers));
+
+        return $response;
+    }
+}
